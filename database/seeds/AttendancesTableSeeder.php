f207ed52a147283db0b07f8832b1b944f480dcfa
fanan <fanan.dala@yahoo.com>
2018-10-28 12:58:52 +0100

    Updated seeders to not seed dummy values for student and attendance

--- database/seeds/AttendancesTableSeeder.php
+++ database/seeds/AttendancesTableSeeder.php
@@ -14,13 +14,13 @@ class AttendancesTableSeeder extends Seeder
     public function run()
     {
         //
-        $students = user::getStudentsIdentification(60);
+        /*$students = user::getStudentsIdentification(60);
 
         foreach($students as $student_id) {
             attendance::create([
                 'event_id' => '1',
                 'student_id' => $student_id->identification,
             ]);
-        }
+        }*/
     }
 }
